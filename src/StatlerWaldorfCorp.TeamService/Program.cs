91a16c72db19bcef0ef936ddc094b5abc9574e9e
Leonardo <yersans@hotmail.com>
2017-11-15 01:24:40 -0800

    Init Project StatlerWaldorfCorp.TeamService

---
+++ src/StatlerWaldorfCorp.TeamService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace hello_world
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                    .UseConfiguration(config)
+                    .Build();
+            host.Run();
+        }
+    }
+}
