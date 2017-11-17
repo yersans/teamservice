21057bf718dd9e40dbc3723a9675fb70ec5e6869
Leonardo <yersans@hotmail.com>
2017-11-16 17:14:52 -0800

    Add CreateTeamAddsTeamToList test.

--- test/StatlerWaldorfCorp.TeamService.Tests/TeamsControllerTest.cs
+++ test/StatlerWaldorfCorp.TeamService.Tests/TeamsControllerTest.cs
@@ -1,6 +1,7 @@
 using Xunit;
 using StatlerWaldorfCorp.TeamService.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StatlerWaldorfCorp.TeamService
 {
@@ -12,7 +13,26 @@ namespace StatlerWaldorfCorp.TeamService
         public void QueryTeamListReturnsCorrectTeams()
         {
             List<Team> teams = new List<Team>(controller.GetAllTeams());
-            Assert.Equal(teams.Count, 2);
+            Assert.Equal(2, teams.Count);
+        }
+
+        [Fact]
+        public async void CreateTeamAddsTeamToList()
+        {
+            TeamsController controller = new TeamsController();
+            var teams = (IEnumerable<Team>)(await controller.GetAllTeams() as ObjectResult).Value;
+            List<Team> original = new List<Team>(teams);
+
+            Team t = new Team("sample");
+            var result = controller.CreateTeam(t);
+
+            var newTeamsRaw = (IEnumerable<Team>)(await controller.GetAllTeams() as ObjectResult).Value;
+            List<Team> newTeams = new List<Team>(newTeamsRaw);
+
+            Assert.Equal(original.Count + 1, newTeams.Count);
+
+            var sampleTeam = newTeams.FirstOrDefault(team => team.Name == "sample");
+            Assert.NotNull(sampleTeam);
         }
     }
 }
